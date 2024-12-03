f02a8d1d3475ef1d5304dd19429b7c5076eebb54
ProgrammerNameHere <mosesbirk@gmail.com>
2024-12-03 21:15:06 +0100

    fourth day solved in two files

---
+++ AoC2015/Day04/solution1.py
@@ -0,0 +1,19 @@
+import hashlib
+import re
+#open the input file and read the contents into a string
+puzzle_file = open('/Users/moses/Documents/advent_of_code/AoC2015/Day04/input.txt', 'r')
+puzzle_input = puzzle_file.read()
+#print(lines)
+puzzle_file.close()
+number = 0
+done = False 
+while not done:
+    hash_seed = puzzle_input + str(number)
+    result = hashlib.md5(hash_seed.encode('utf-8'))
+    ###print(hash_seed)
+    ##print(number)
+    #print(result.hexdigest())
+    if re.findall('^0{5,}', result.hexdigest()) != []:
+        print(number)
+        done = True
+    number += 1
