8069b7e36f748c7e6b90fc718b0f7ff3973effbc
ProgrammerNameHere <mosesbirk@gmail.com>
2024-12-04 18:25:45 +0100

    Figured out almost everything for part1 of 2015/07, just have to keep my 16 bit numbers from overflowing somehow...

--- AoC2015/Day07/part1.py
+++ AoC2015/Day07/part1.py
@@ -1,31 +1,55 @@
 
 #open the input file and read the contents into a string
-puzzle_file = open('/Users/moses/Documents/advent_of_code/AoC2015/Day07/input.txt', 'r')
+puzzle_file = open('/Users/moses/Documents/advent_of_code/AoC2015/Day07/test_input.txt', 'r')
 puzzle_input = puzzle_file.read().splitlines()
 #print(lines)
 puzzle_file.close()
 wires ={
-    'a': 0
+
 }
 for line in puzzle_input:
-    line = line.split()
-    input1 = ''
-    input2 = ''
-    gate = ''
-    target = ''
-    
-    target = line[-1]
-    #check if the target wire is in the dictionary already
-    if wires.get(target, '') == '':
-        wires.update({target: 0})
-    #check what operation to perform
-    #check for a gate
-    if line[1] == '->':
-        #direct assignment from a different wire
-        wires.get(line[0],line[0])
+    #print(wires)
+    print(line)
+    line = line.split(' -> ')
+    target = line[1]
+    input = line[0].split()
+    line_rebuild = ' -> '.join(line)
+    # direct input or input from wire
+    if len(input) == 1:
 
-        wires[target] = wires[line[0]]
-        #direct assignment from a value
-    #differentiate the gates
+        if wires.get(input[0], True) and not input[0].isdigit():
+            puzzle_input.append(line_rebuild)
+            break
+        source = wires.get(input[0], int(input[0]))
+        wires.update({target: source})
+    elif len(input) == 2: # NOT gate
+        #if the wire is not active yet, add this instruction to the end of the list
+        if wires.get(input[1], None) == None:
+            puzzle_input.append(line_rebuild)
+            break
+        wires.update({target: (~ wires[input[1]])})
+    else: # all other logic gates
+        gate = input[1]
+        left_of_gate, right_of_gate = input[0], input[2]
+        if gate == 'AND':
+            if wires.get(left_of_gate, True) == True or wires.get(right_of_gate, True) == True:
+                puzzle_input.append(line_rebuild)
+                break
+            wires.update({target: (wires[left_of_gate] & wires[right_of_gate])})
+        elif gate == 'OR':
+            if wires.get(left_of_gate, True) == True or wires.get(right_of_gate, True) == True:
+                puzzle_input.append(line_rebuild)
+                break
+            wires.update({target: (wires[left_of_gate] | wires[right_of_gate])})
+        elif gate == 'LSHIFT':
+            if wires.get(left_of_gate, True) == True:
+                puzzle_input.append(line_rebuild)
+                break
+            wires.update({target: (wires[left_of_gate] << int(right_of_gate))})
+        elif gate == 'RSHIFT':
+            if wires.get(left_of_gate, True) == True:
+                puzzle_input.append(line_rebuild)
+                break
+            wires.update({target: (wires[left_of_gate] >> int(right_of_gate))})
 
-print(wires['a'])
+print(wires)
