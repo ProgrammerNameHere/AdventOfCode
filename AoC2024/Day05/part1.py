a0d6ed4e415f9868e0c5a5708835b17fe939210c
ProgrammerNameHere <mosesbirk@gmail.com>
2024-12-05 15:57:58 +0100

    changed 2024/05 paths to reflect that the input is in the same folder as the .py file

--- AoC2024/Day05/part1.py
+++ AoC2024/Day05/part1.py
@@ -1,4 +1,4 @@
-puzzle_file = open('/Users/moses/Documents//advent_of_code/AoC2024/Day05/input.txt', 'r')
+puzzle_file = open('input.txt', 'r')
 puzzle_input = puzzle_file.read().splitlines()
 #print(puzzle_input)
 puzzle_file.close()
