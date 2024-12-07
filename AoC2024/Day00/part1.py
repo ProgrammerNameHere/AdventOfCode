ba9380edffc5e872c78989cfd110e322a708b34c
ProgrammerNameHere <mosesbirk@gmail.com>
2024-12-07 13:44:20 +0100

    changed the Day00 Template to reflect the input file being in the same folder as the python file

--- AoC2024/Day00/part1.py
+++ AoC2024/Day00/part1.py
@@ -1,4 +1,4 @@
-puzzle_file = open('/Users/moses/Documents/PythonCode/advent_of_code/AoC2024/Day00/input.txt', 'r')
+puzzle_file = open('./input.txt', 'r')
 puzzle_input = puzzle_file.read()
 #print(puzzle_input)
 puzzle_file.close()
