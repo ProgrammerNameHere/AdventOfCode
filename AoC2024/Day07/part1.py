07b250e6ba267011f0877f2327ed9d5f12366832
ProgrammerNameHere <mosesbirk@gmail.com>
2024-12-08 11:02:19 +0100

    did a bit more work on the recursive solution

--- AoC2024/Day07/part1.py
+++ AoC2024/Day07/part1.py
@@ -5,13 +5,15 @@ puzzle_file.close()
 # define global variables
 calibration_result = 0
 
-def is_solvable(test, calculation, rolling_total=0)
+def is_solvable(test, calculation, rolling_total=0):
     if calculation == []:
         if rolling_total == test:
             return True
         else:
             return False
-    
+    plus_solution = is_solvable(test, calculation[1:], rolling_total + calculation[0])
+    times_solution = is_solvable(test, calculation[1:], (1 if rolling_total == 0 else rolling_total) * calculation[0])
+    return plus_solution, times_solution
     
 
 # check each calibration if it is solvable
@@ -19,14 +21,13 @@ def is_solvable(test, calculation, rolling_total=0)
 # calibration result
 for calculation in puzzle_input:
     test_value, equation = calculation.split(':')
-    numbers_in_equation = equation.strip().split()
+    numbers_in_equation = list(map(int, equation.strip().split()))
     
-    if is_solvable(test_value, numbers_in_equation):
-        calibration_result += test_value
+    if any(is_solvable(test_value, numbers_in_equation)):
+        calibration_result += int(test_value)
 
 
 
 
 if __name__ == '__main__':
-
     print(calibration_result)
